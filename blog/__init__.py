4e733bda8d38049a3769f4dfed846ac8441476c3
Bob Braico <bb071988@gmail.com>
2015-05-11 02:04:04 +0000

    initial commit

---
+++ blog/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import os # had to add this don't believe it was part of exercise
+
+app = Flask(__name__)
+
+config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
+app.config.from_object(config_path)
+
+from . import views
+from . import filters
