532526f1fb763c08fbe7c0dbbfc4f83c1ab8b12b
bb071988 <bb071988@gmail.com>
2015-08-04 23:48:55 +0000

    trying the travis environment variable page

--- blog/config.py
+++ blog/config.py
@@ -8,8 +8,8 @@ class DevelopmentConfig(object):
 ### Adding testing config class for lesson 5.2
 
 class TestingConfig(object):
-   # SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:action@localhost:5432/blogful-test"
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:action@localhost:5432/blogful-test"
+    #SQLALCHEMY_DATABASE_URI = "postgresql://postgres: @localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
     
@@ -19,6 +19,6 @@ class TravisConfig(object):
     DEBUG = False
     SECRET_KEY = "Not secret"
     
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
+    
     
    
