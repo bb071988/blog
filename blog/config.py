49141e5e7e03b472b25952e92b319e67b328f664
bb071988 <bb071988@gmail.com>
2015-08-04 22:38:12 +0000

    updated yml for splinter install again2

--- blog/config.py
+++ blog/config.py
@@ -8,7 +8,8 @@ class DevelopmentConfig(object):
 ### Adding testing config class for lesson 5.2
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:action@localhost:5432/blogful-test"
+   # SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:action@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
     
